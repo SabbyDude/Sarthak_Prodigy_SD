14a199623e84a4366dc2ab37065b1e470ee0593a
SabbyDude <sarthakpiyushagra@gmail.com>
2024-06-29 23:21:28 +0530

    new changes

--- PRODIGY_SD_02.py
+++ PRODIGY_SD_02.py
@@ -1,5 +1,10 @@
 import random
-guess = random.randint(1,100)
+start = int(input("What should be the starting range of the numbers? -->"))
+endit = int(input("What should be the ending range of the numbers? -->"))
+if (start >= endit):
+	print("The ending number is either same or smaller than the beginning number which is not allowed")
+	exit(0)
+guess = random.randint(start,endit)
 count = 1
 your = int(input(f"Enter Guess No.{count}:"))
 while(guess != your):
